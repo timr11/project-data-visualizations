fe565e25fbf32275433875d6908e6ab578bf7393
timr11 <timromanski@gmail.com>
2023-07-18 15:25:23 -0400

    build: exclude deps from output bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require("path");
+const pkg = require("./package.json");
+
+env = process.env;
+const NODE_ENV = env.NODE_ENV;
+const MIN = env.MIN;
+const PROD = NODE_ENV === "production";
 
 module.exports = {
 	mode: "production", // Set the mode to 'production' for minification
 	entry: "./src/app.ts", // Entry point of your application
 	output: {
-		filename: "app.min.js", // Output file name
+		filename: pkg.name + ".js", // Output file name
 		path: path.resolve(__dirname, "bin"), // Output directory path
 	},
 	resolve: {
@@ -18,5 +24,9 @@ module.exports = {
 				use: "ts-loader", // Use ts-loader for transpiling TypeScript
 			},
 		],
+	},
+	externals: PROD ? Object.keys(pkg.dependencies || {}) : [],
+	optimization: {
+		minimize: !!MIN,
 	},
 };
