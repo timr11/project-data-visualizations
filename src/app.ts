4b2b651e40c49bc3eb660d2701a5627dbc3db11d
timr11 <timromanski@gmail.com>
2023-07-20 16:59:00 -0400

    feat: Gets cytoscape ts code to run in an ipynb
    Cytoscape JS code successfully runs in an ipynb.
    !!NOTE!!: The ipynb graph renders as a very narrow
    window. The fulfillment plan icons are also not
    rendering, they were downloaded as PNGs which can't be
    embedded directly to the JS build artifact afaik and
    can't be accessed by the notebookjs generated HTML
    file that renders the graph in the ipynb. If they're
    SVGs it should work.

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,76 @@
 import cytoscape from "cytoscape";
+import popper from "cytoscape-popper";
+import tippy from "tippy.js";
 
-document.addEventListener("DOMContentLoaded", function () {
+cytoscape.use(popper);
+
+export function drawGraph(div_id: string, data: any) {
+	// @ts-ignore
+	var dbgr = new CommAPI("debugger", (ret) => {});
+	// @ts-ignore
+	dbgr.call({ data: "woo woo" });
+
+	var cyDiv = document.createElement("div") as HTMLElement;
+	cyDiv.id = "cy";
+	cyDiv.className = "fullscreen";
+	document.querySelector(div_id)?.appendChild(cyDiv);
+
 	var cy = cytoscape({
 		container: document.getElementById("cy"),
-		elements: [
+		elements:
+			// data["elements"],
 			{
-				data: { id: "Devhawk Engineering" },
-				classes: "maker",
+				nodes: [
+					{
+						data: {
+							id: "Devhawk Engineering",
+							product: {
+								identifier: "Q15026",
+								description: "chair",
+							},
+						},
+						classes: "maker",
+					},
+					{
+						data: {
+							id: "Robert's Chair Parts",
+							product: {
+								identifier: "Q15026",
+								description: "chair",
+							},
+						},
+						classes: "supplier",
+					},
+				],
+				edges: [
+					{
+						data: {
+							id: "e1",
+							source: "Devhawk Engineering",
+							target: "Robert's Chair Parts",
+						},
+					},
+				],
 			},
-			{
-				data: { id: "Robert's Chair Parts" },
-				classes: "supplier",
-			},
-			{
-				data: {
-					id: "e1",
-					source: "Devhawk Engineering",
-					target: "Robert's Chair Parts",
-				},
-			},
-		],
 		style: [
 			{
 				selector: ".maker",
 				style: {
-					"background-image": "url(../public/assets/Maker.png)",
 					shape: "rectangle",
-					"background-fit": "contain",
-					"background-opacity": 0,
+					backgroundColor: "red",
+					// "background-fit": "contain",
+					// "background-opacity": 0,
+					// "background-image": "url(../public/assets/Maker.png)",
 				},
 			},
 			{
 				selector: ".supplier",
 				style: {
-					"background-image": "url(../public/assets/Supplier.png)",
 					shape: "rectangle",
-					"background-fit": "contain",
-					"background-opacity": 0,
+					backgroundColor: "blue",
+					// "background-fit": "contain",
+					// "background-opacity": 0,
+					// "background-image": "url(../public/assets/Supplier.png)",
 				},
 			},
 			{
@@ -56,4 +90,62 @@ document.addEventListener("DOMContentLoaded", function () {
 			cols: 1,
 		},
 	});
-});
+
+	cy.on("click", "node", (event) => {
+		var node: cytoscape.NodeSingular = event.target;
+		console.log("Clicked " + node.data("id"));
+
+		var suppliers = cy.$(".supplier");
+		suppliers.forEach((node) => {
+			if (node.hidden()) {
+				node.css({ visibility: "visible" });
+				node.connectedEdges().forEach((edge) => {
+					edge.css({ visibility: "visible" });
+				});
+			} else {
+				node.css({ visibility: "hidden" });
+				node.connectedEdges().forEach((edge) => {
+					edge.css({ visibility: "hidden" });
+				});
+			}
+		});
+	});
+
+	var makeTippy = function (node: cytoscape.NodeSingular, text: string) {
+		var ref = node.popperRef();
+		var dummyDomEle = document.createElement("div");
+
+		var tip = tippy(dummyDomEle, {
+			getReferenceClientRect: ref.getBoundingClientRect,
+			trigger: "manual",
+			content: function () {
+				var div = document.createElement("div");
+
+				div.innerHTML = text;
+
+				return div;
+			},
+			arrow: true,
+			placement: "bottom",
+			hideOnClick: false,
+			sticky: "reference",
+
+			interactive: true,
+			appendTo: document.body,
+		});
+
+		node.on("mouseover", () => tip.show());
+		node.on("mouseout", () => tip.hide());
+
+		return tip;
+	};
+
+	cy.nodes().forEach((node) => {
+		var product = node.data()["product"];
+		if (node.classes().includes("maker"))
+			makeTippy(
+				node,
+				`ID: ${product["identifier"]}<br/>Description: ${product["description"]}`
+			);
+	});
+}
--- webpack.config.js
+++ webpack.config.js
@@ -10,22 +10,29 @@ module.exports = {
 	mode: "production", // Set the mode to 'production' for minification
 	entry: "./src/app.ts", // Entry point of your application
 	output: {
-		filename: pkg.name + ".js", // Output file name
+		filename: `${pkg.name}.js`, // Output file name
 		path: path.resolve(__dirname, "bin"), // Output directory path
+		library: "projectdatavisualizations",
 	},
 	resolve: {
-		extensions: [".ts", ".js"], // Add TypeScript and JavaScript as resolvable extensions
+		extensions: [".tsx", ".ts", ".js", ".png"], // Add TypeScript and JavaScript as resolvable extensions
 	},
 	module: {
 		rules: [
 			{
 				test: /\.ts$/, // Apply the following rules to TypeScript files
-				exclude: /node_modules/, // Exclude node_modules directory from processing
 				use: "ts-loader", // Use ts-loader for transpiling TypeScript
+				exclude: /node_modules/, // Exclude node_modules directory from processing
 			},
 		],
 	},
-	externals: PROD ? Object.keys(pkg.dependencies || {}) : [],
+	devServer: {
+		static: {
+			directory: __dirname,
+		},
+		compress: true,
+		port: 8000,
+	},
 	optimization: {
 		minimize: !!MIN,
 	},
